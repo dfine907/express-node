e99f2429f5c737e5dacede6197d06a4dbfc2fdf5
Dina <dinagiacobbi@gmail.com>
2024-09-12 12:58:34 -0800

    Using morgan and adding more middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,40 +1,17 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-//request => middleware => response
-const authorize = require('./authorize')
+const { people } = require('./data')
 
-app.use([logger, authorize])
+// static methods
 
-//  MOVED to it's own file ---- >
-// const logger = (req,res, next) => {
-//   const method = req.method
-//   const url = req.url
-//   const time = new Date().getFullYear()
-//   console.log(method, url, time)
-//   // res.send('Testing ')
-//   next()
-// }
+app.use(express.static('./methods-public'))
 
-app.get('/', (req, res) => {
-  res.send('<h2>HOME PAGE</h2>')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About Page')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products Page')
-})
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  
-  res.send('Items Page')
-})
-app.get('/api/customers', (req, res) => {
-  res.send('Customers Page')
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success: true, data: people})
 })
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
+
+
