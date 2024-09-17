fe00ac7dbff16742404d572d55e5f52294db36b4
Dina <dinagiacobbi@gmail.com>
2024-09-17 10:44:09 -0800

    Adding new server code for Task App

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,22 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Routes:
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App')
+})
+
+/* Pseudo Code - what routes I need
+app.get('/api/v1/tasks')   - gets ALL tasks
+app.post('/api/v1/tasks')   - creates a new task
+app.get('/api/v1/tasks/:id') - get one task
+app.patch('/api/v1/tasks/:id') - upate one task
+app.delete('/api/v1/tasks/:id') - delte one task
+
+*/
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port} ...`)
+})
