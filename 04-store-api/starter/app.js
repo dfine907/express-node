62f725857878974d608d41894ed77b4d06e420b8
Dina <dinagiacobbi@gmail.com>
2024-09-26 11:06:23 -0800

    Adding MongoDB connection string functions

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
@@ -26,6 +28,7 @@ const port =process.env.PORT || 3000
 const start = async () => {
     try {
         //connect DB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server listening on PORT ${port} ...`))
     } catch (error) {
         console.log(error)
