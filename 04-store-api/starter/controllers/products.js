ce38a4d5b13a817835962c271ab2065875f5c7cb
Dina <dinagiacobbi@gmail.com>
2024-09-27 11:40:18 -0800

    Setting up model schema

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,14 +1,12 @@
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
-    // res.status(200).json({ msg: "productions testing route" });
-  };
-  
-  const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "productions route" });
-  };
-  
-  module.exports = {
-    getAllProductsStatic,
-    getAllProducts,
-  };
-  
+  throw new Error('testing async errors')
+}
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: 'productions route' })
+}
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
+}
