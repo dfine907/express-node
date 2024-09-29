9f17dae7a486b17b947d7edab901b7fec03d505b
Dina <dinagiacobbi@gmail.com>
2024-09-29 08:19:38 -0800

    Populating data to connect to DB

--- 04-store-api/starter/populate.js
+++ 04-store-api/starter/populate.js
@@ -6,3 +6,15 @@ const Product = require('./models/product')
 
 const jsonProducts = require('./products.json')
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    await Product.deleteMany()
+    await Product.create(jsonProducts)
+    console.log('success!!')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
