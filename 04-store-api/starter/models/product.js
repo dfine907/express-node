2a55552f9d94a1c95a13e24a79ef4cca541e0423
Dina <dinagiacobbi@gmail.com>
2024-09-28 17:30:31 -0800

    Adding json products

--- 04-store-api/starter/models/product.js
+++ 04-store-api/starter/models/product.js
@@ -25,7 +25,6 @@ const productSchema = new mongoose.Schema({
 
     company: {
         type: String,
-        // enum: ['ikea', 'liddy', 'caressa', 'marcos']
         enum: {
             values: ['ikea', 'liddy', 'caressa', 'marcos'],
             message:'{VALUE} is not supported'
