6c35ed8a8a3e0dac523f104d1dccec7116704af7
Dina <dinagiacobbi@gmail.com>
2024-10-05 10:47:01 -0800

    Adding sort functionality for alpha

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,32 +2,41 @@ const Product = require('../models/product')
 
 //TESTING:
 const getAllProductsStatic = async (req, res) => {
-  const search = 'a'
-  const products = await Product.find({
-    name: {$regex:search, $options:'i'}
-  })
+  const products = await Product.find({}).sort('name')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
-  //now set up a new object:
+  const { featured, company, name, sort } = req.query
+
   const queryObject = {}
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
-  if(company){
+  if (company) {
     queryObject.company = company
   }
 
-  if(name){
-    queryObject.name = {$regex: name, $options:'i'}
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
   }
-  console.log(queryObject)
+  // if (name) {
+  //   queryObject.name_lowercase = name.toLowerCase()
+  // }
 
-  const products = await Product.find(queryObject)
 
+  // const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createAt')
+  }
+
+  const products = await result
   // res.status(200).json({ msg: 'productions route' })
   res.status(200).json({ products, nbHits: products.length })
 }
@@ -36,8 +45,6 @@ module.exports = {
   getAllProductsStatic,
   getAllProducts,
 }
-
-
 
 //ORGINAL SIMPLE:
 // const Product = require('../models/product')
