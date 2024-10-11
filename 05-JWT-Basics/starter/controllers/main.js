64bf3b9aafc9279378801a575913d9c67437f925
Dina <dinagiacobbi@gmail.com>
2024-10-11 13:27:34 -0800

    Adding login dashboard routes

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,10 +1,10 @@
-const login = async ((req, res) => {
+const login = async (req, res) => {
     res.send(`Fake Login/Register/SignUp Route`)
-})
+}
 
 const dashboard = async (req, res) => {
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json( {msg: `Hello John Doe`, secret: `Here is your authorized data. Your i lucky number is ${luckyNumber}`})
+    res.status(200).json( {msg: `Hello John Doe`, secret: `Here is your authorized data. Your  lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
