68795f577dc2488a7e4aff738f097bfb427a6c2e
Dina <dinagiacobbi@gmail.com>
2024-10-14 17:05:34 -0800

    JWT additions

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,21 +1,25 @@
+
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
-const {username, password} = req.body
+  const { username, password } = req.body
 
-if(!username || !password){
-throw new CustomAPIError('Please provide email and password', 400)
-}
-    console.log(username, password)
-    res.send(`Fake Login/Register/SignUp Route`)
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400)
+  }
+  console.log(username, password)
+  res.send(`Fake Login/Register/SignUp Route`)
 }
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json( {msg: `Hello John Doe`, secret: `Here is your authorized data. Your  lucky number is ${luckyNumber}`})
+  const luckyNumber = Math.floor(Math.random() * 100)
+  res.status(200).json({
+    msg: `Hello John Doe`,
+    secret: `Here is your authorized data. Your  lucky number is ${luckyNumber}`,
+  })
 }
 
 module.exports = {
-    login,
-    dashboard
+  login,
+  dashboard,
 }
