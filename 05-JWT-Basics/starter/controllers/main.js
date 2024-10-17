ec7611782bd969e0e42f0550e0cec351930281a8
Dina <dinagiacobbi@gmail.com>
2024-10-16 17:13:33 -0800

    Editing token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,5 @@
 
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
@@ -7,8 +8,14 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError('Please provide email and password', 400)
   }
-  console.log(username, password)
-  res.send(`Fake Login/Register/SignUp Route`)
+
+  // this id is just for testing
+  const id = new Date().getDate()
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+  res.status(200).json({ msg: `User created`, token })
 }
 
 const dashboard = async (req, res) => {
@@ -23,3 +30,4 @@ module.exports = {
   login,
   dashboard,
 }
+
