bda8806bf591f446142dc19ca8d554163b1f02c2
Dina <dinagiacobbi@gmail.com>
2024-10-22 12:00:39 -0800

    Setting up error classes, adding status codes

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UnauthenticatedError = require('./unathenticated')
+
+module.exports = {
+    CustomAPIError,
+    BadRequest,
+    UnauthenticatedError
+}
