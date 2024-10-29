7f7d64cd50160ffaad4972dc4e85213f8872f29d
Dina <dinagiacobbi@gmail.com>
2024-10-29 12:56:05 -0800

    Adding method to schema to compare password match

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 const register = async (req, res) => {
   /* BELOW IS THE CODE TO HASH BEFORE USING MONGOOSE - START...
@@ -23,7 +24,22 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  res.send('Login User!')
+  // res.send('Login User!')
+  const { email, password} = req.body
+  if(!email || !password){
+    throw new BadRequestError("Please provide email and password")
+  }
+  const user = await User.findOne({email})
+  if(!user){
+    throw new UnauthenticatedError("Invalid credentials")
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if(!isPasswordCorrect){
+    throw new UnauthenticatedError("Invalid credentials")
+  }
+  // compare pasword
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({user: { name: user.name },token})
 }
 
 module.exports = {
