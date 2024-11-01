b9ee2c1eae54d7f2a1748542eafda28fcc46bf09
Dina <dinagiacobbi@gmail.com>
2024-11-01 13:03:35 -0800

    Updating controller for getAllJobs, createJob

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,25 +1,36 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+//get all Jobs associated with one user
 const getAllJobs = async (req, res) => {
-    console.log(req)
-    
-  res.send('Get All Jobs')
+  const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+  res.status(StatusCodes.OK).json({ jobs, count:jobs.length })
 }
+
 const getJob = async (req, res) => {
   res.send('get a job')
 }
+
 const createJob = async (req, res) => {
-  res.json(req.user)
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
+
 const updateJob = async (req, res) => {
   res.send('update a job')
 }
+
 const deleteJob = async (req, res) => {
   res.send('delete a job')
 }
+
 
 module.exports = {
   getAllJobs,
   getJob,
   createJob,
   updateJob,
-  deleteJob
+  deleteJob,
 }
