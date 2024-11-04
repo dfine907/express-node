a9c94c31cefd0ddc649f4faa9ede7d90f79d80fc
Dina <dinagiacobbi@gmail.com>
2024-11-04 09:54:15 -0900

    Adding get single job controller function

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -9,7 +9,16 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-  res.send('get a job')
+  //destructure -reminder params provided by express
+  const { user:{userId}, params:{id:jobId}} = req
+  //now find the ONE job - jobId
+  const job = await Job.findOne({
+    _id:jobId, createdBy:userId
+  })
+  if(!job){
+    throw new NotFoundError(`No job found with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json( {job})
 }
 
 const createJob = async (req, res) => {
