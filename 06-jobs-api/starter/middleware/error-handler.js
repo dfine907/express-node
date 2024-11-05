ba211c2d035db446979a494946f3930935f377fe
Dina <dinagiacobbi@gmail.com>
2024-11-05 11:34:48 -0900

    Refactoring validation error

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
@@ -8,11 +7,22 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong. Try again',
   }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  if (err.name === 'ValidationError') {
+    console.log(Object.values(err.errors))
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ')
+    customError.statusCode = 400
   }
+
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field. Please chose another value.`
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field. Please chose another value.`
     customError.statusCode = 400
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
