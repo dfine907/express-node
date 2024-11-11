fc9c577e0617de56a33d69b8f0d237ee79954259
Dina <dinagiacobbi@gmail.com>
2024-11-11 10:45:16 -0900

    Adding product and uploads controllers

---
+++ 07-file-upload/starter/controllers/productController.js
@@ -0,0 +1,16 @@
+const Product = require('../models/Product')
+const { StatusCodes } = require('http-status-codes')
+
+const createProduct = async (req, res) => {
+    res.send('Create product')
+}
+
+const getAllProducts = async (req, res) => {
+    res.send('List of products')
+}
+
+
+module.exports = {
+    createProduct,
+    getAllProducts
+}
