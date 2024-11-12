ffdd4ea3ba7d059e271e9c3629f2cabf50df98c5
Dina <dinagiacobbi@gmail.com>
2024-11-12 13:37:12 -0900

    Adding new routes for products and uploads

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,10 +1,9 @@
 const { StatusCodes } = require('http-status-codes')
 
 const uploadProductImage = async (req, res) => {
-    res.send('Upload product image')
+  res.send('Upload product image')
 }
 
-
 module.exports = {
-    uploadProductImage
+  uploadProductImage,
 }
