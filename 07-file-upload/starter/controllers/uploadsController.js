16ce16f9e510f62ea6de6a104bf59cae77f24208
Dina <dinagiacobbi@gmail.com>
2024-11-15 09:39:13 -0900

    Adding images for file express

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,9 +1,20 @@
+const path = require('path')
+
 const { StatusCodes } = require('http-status-codes')
 
 const uploadProductImage = async (req, res) => {
-  console.log(req.files);
+  // console.log(req.files)
+  const productImage = req.files.image
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  )
   
-  res.send('Upload product image')
+  await productImage.mv(imagePath)
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } })
 }
 
 module.exports = {
