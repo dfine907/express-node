59bae821d171fc5bb7d6aa6d85c68d6c9ce90b72
Dina <dinagiacobbi@gmail.com>
2024-11-16 12:23:27 -0900

    Adding custom error message for uploads

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,16 +1,30 @@
 const path = require('path')
-
 const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
 
 const uploadProductImage = async (req, res) => {
   // console.log(req.files)
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file uploaded')
+  }
+
   const productImage = req.files.image
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please upload an image')
+  }
+
+  const maxSize = 1024 * 1024
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1KB'
+    )
+  }
 
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
   )
-  
+
   await productImage.mv(imagePath)
   return res
     .status(StatusCodes.OK)
