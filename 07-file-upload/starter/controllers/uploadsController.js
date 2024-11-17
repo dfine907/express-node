95e3b1c5d5b4e702324e3c8140e9930030ce41d7
Dina <dinagiacobbi@gmail.com>
2024-11-17 11:45:31 -0900

    Adding cloudinary code for image storage

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const cloudinary = require('cloudinary').v2
 
-const uploadProductImage = async (req, res) => {
+const fs = require('fs')
+
+const uploadProductImageLocal = async (req, res) => {
   // console.log(req.files)
   if (!req.files) {
     throw new CustomError.BadRequestError('No file uploaded')
@@ -29,6 +32,18 @@ const uploadProductImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } })
+}
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'file-upload',
+    }
+  )
+  fs.unlinkSync(req.files.image.tempFilePath)
+  return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
 }
 
 module.exports = {
