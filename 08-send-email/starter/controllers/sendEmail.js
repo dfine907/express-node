bd7098599e78e4b05c6fdda53c68b465ca5fa48b
Dina <dinagiacobbi@gmail.com>
2024-11-18 12:47:27 -0900

    Adding email mailer

--- 08-send-email/starter/controllers/sendEmail.js
+++ 08-send-email/starter/controllers/sendEmail.js
@@ -1,5 +1,25 @@
-const sendEmail = async (req, res)=> {
-    res.send("Send email function")
+const nodemailer = require('nodemailer')
+
+const sendEmail = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount()
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'roscoe40@ethereal.email',
+      pass: 'RPCm3kH8r1hn7AS4a8',
+    },
+  })
+
+  const info = await transporter.sendMail({
+    from: '"Dina Fine 👻" <dinafine907@gmail.com>', // sender address
+    to: 'bar@example.com, baz@example.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    html: '<h2>Sending Emails with NodeJS! 💥 </h2>', // html body
+  })
+
+  res.json(info)
 }
 
 module.exports = sendEmail
