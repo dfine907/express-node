4cbe69ec324eb97275444a226ab81a22fe867d6b
Dina <dinagiacobbi@gmail.com>
2024-11-20 09:51:02 -0900

    Adding stripe payment processing

--- 09-stripe-payment/starter/controllers/stripeController.js
+++ 09-stripe-payment/starter/controllers/stripeController.js
@@ -1,6 +1,23 @@
-const stripe = async (req,res) => {
-    console.log(req.body)
-    res.send('stripe route')
+
+
+const stripe = require('stripe')(process.env.STRIPE_API_SECRET)
+
+const stripeController = async (req, res) => {
+  // console.log(req.body)
+  const { purchase, total_amount, shipping_fee } = req.body
+
+  //send total to stripe
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee
+  }
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'usd',
+  })
+  console.log(paymentIntent)
+  res.json({ clientSecret: paymentIntent.client_secret })
+  //   res.send('stripe route')
 }
 
-module.exports = stripe
+module.exports = stripeController
