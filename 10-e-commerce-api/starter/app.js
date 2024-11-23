4700c82f2890800b6105c4b252832c1d54a2ed26
Dina <dinagiacobbi@gmail.com>
2024-11-23 08:46:03 -0900

    Adding new server code

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,14 @@
-console.log('E-Commerce API');
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 8000
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Listening on port ${port}... `))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
