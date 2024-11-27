7d528058568900ee32f10ac43b0e7d4a4153ea3f
Dina <dinagiacobbi@gmail.com>
2024-11-27 06:24:41 -0900

    Adding unique property validator for duplidate email

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,25 +1,31 @@
-const User = require('../models/User');
-const  { StatusCodes } = require('http-status-codes')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 //utils here
 
 const register = async (req, res) => {
-    //everything is accessible in req.body because of json middleware in app.js
-    const user = await User.create(req.body)
-    res.status(StatusCodes.CREATED).json( {user})
-    
+  //everything is accessible in req.body because of json middleware in app.js
+  const {email} = req.body
+
+  const emailAlreadyExists = await User.findOne({email})
+    if(emailAlreadyExists){
+        throw new CustomError.BadRequestError("Email already in use")
+    }
+
+  const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
-    res.send ("Login user")
+  res.send('Login user')
 }
 
 const logout = async (req, res) => {
-    res.send ("Logout user")
+  res.send('Logout user')
 }
 
 module.exports = {
-    register,
-    login,
-    logout
+  register,
+  login,
+  logout,
 }
