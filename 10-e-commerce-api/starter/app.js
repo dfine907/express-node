c676b18850b150aa544d7641ddfb723148031e4a
Dina <dinagiacobbi@gmail.com>
2024-11-28 12:54:24 -0900

    Setting up cookies

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -7,6 +7,9 @@ const app = express()
 //rest of the packages
 const morgan = require('morgan')
 
+//cookies
+const cookieParser = require('cookie-parser')
+
 //database
 const connectDB = require('./db/connect')
 
@@ -19,8 +22,16 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.get('/', (req, res) => {
+  res.send('E-commerce API')
+})
+
+app.get('/api/v1', (req, res) => {
+  console.log("COOKIES- ", req.cookies);
+  
   res.send('E-commerce API')
 })
 
