225af3c811cc3d30958e52e3dc4fb27a971f881e
Dina <dinagiacobbi@gmail.com>
2024-12-02 07:50:34 -0900

    Fixing small creating token for user

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,58 +1,68 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
-const {
-  attachCookiesToResponse,
-  createTokenUser,
-} = require('../utils')
+const jwt = require('jsonwebtoken')
+const { attachCookiesToResponse } = require('../utils')
 
 const register = async (req, res) => {
+  //everything is accessible in req.body because of json middleware in app.js
   const { email, name, password } = req.body
 
   const emailAlreadyExists = await User.findOne({ email })
   if (emailAlreadyExists) {
-    throw new CustomError.BadRequestError('Email already exists')
+    throw new CustomError.BadRequestError('Email already in use')
   }
 
-  // first registered user is an admin
+  //First registered user is an admin
   const isFirstAccount = (await User.countDocuments({})) === 0
   const role = isFirstAccount ? 'admin' : 'user'
 
-  const user = await User.create({ name, email, password, role })
-  const tokenUser = createTokenUser(user)
+  const user = await User.create({ email, name, password, role })
+  const tokenUser = {
+    name: user.name,
+    userId: user._id,
+    role: user.role,
+  }
   attachCookiesToResponse({ res, user: tokenUser })
   res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
+
+
 const login = async (req, res) => {
-  const { email, password } = req.body
 
-  if (!email || !password) {
-    throw new CustomError.BadRequestError(
-      'Please provide email and password'
-    )
+  // firsrt destructure req.body has it all:
+  const {email, password} = req.body
+  if(!email || !password){
+    throw new CustomError.BadRequestError('Please provide email and password')
   }
+  //check if there is a user in DB
   const user = await User.findOne({ email })
-
-  if (!user) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials')
+  if(!user){
+    throw new CustomError.UnauthenticatedError('Invalid credentials')
   }
+  //check the right password
   const isPasswordCorrect = await user.comparePassword(password)
-  if (!isPasswordCorrect) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials')
+  if(!isPasswordCorrect){
+    throw new CustomError.UnauthenticatedError('Invalid credentials')
   }
-  const tokenUser = createTokenUser(user)
+  const tokenUser = {
+    name: user.name,
+    userId: user._id,
+    role: user.role,
+  }
   attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
 
-  res.status(StatusCodes.OK).json({ user: tokenUser })
+  // res.send('Login user')
 }
 
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
-    expires: new Date(Date.now() + 1000),
+    expires:new Date(Date.now() + 5 * 1000)
   })
-  //for dev testing
-  res.status(StatusCodes.OK).json({ msg: 'user logged out!' })
+  res.status(StatusCodes.OK).json( {msg: 'User logged out!!'})
+  // res.send('Logout user')
 }
 
 module.exports = {
