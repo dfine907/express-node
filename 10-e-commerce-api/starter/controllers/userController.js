91ce568f55e7d7a002a45882133deab60094ef36
Dina <dinagiacobbi@gmail.com>
2024-12-07 13:02:16 -0900

    Adding update user with save hook

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -1,7 +1,10 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
-const { createTokenUser, attachCookiesToResponse } = require('../utils')
+const {
+  createTokenUser,
+  attachCookiesToResponse,
+} = require('../utils')
 
 const getAllUsers = async (req, res) => {
   // console.log(req.user)
@@ -25,22 +28,41 @@ const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user })
 }
 
-const updateUser = async (req, res) => {
-  const { name, email } = req.body
-  if (!name || !email) {
-    throw new CustomError.BadRequestError(
-      'Please provide email and password'
-    )
-  }
+// * * * *  UPDATE USER WITH findOneAndUpdate * * * *
+// const updateUser = async (req, res) => {
+//   const { name, email } = req.body
+//   if (!name || !email) {
+//     throw new CustomError.BadRequestError(
+//       'Please provide email and password'
+//     )
+//   }
 
-  const user = await User.findOneAndUpdate(
-    { _id: req.user.userId },
-    { email, name },
-    { new: true, runValidators: true }
-  )
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { email, name },
+//     { new: true, runValidators: true }
+//   )
+
+//   const tokenUser = createTokenUser(user)
+//   attachCookiesToResponse({ res, user: tokenUser })
+//   res.status(StatusCodes.OK).json({ user: tokenUser })
+// }
+
+// * * * * Update USER with user.save method * * * *
+const updateUser = async (req, res) => {
+  const { email, name } = req.body
+  if (!email || !name) {
+    throw new CustomError.BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ _id: req.user.userId })
+
+  user.email = email
+  user.name = name
+
+  await user.save()
 
   const tokenUser = createTokenUser(user)
-  attachCookiesToResponse( {res, user:tokenUser})
+  attachCookiesToResponse({ res, user: tokenUser })
   res.status(StatusCodes.OK).json({ user: tokenUser })
 }
 
