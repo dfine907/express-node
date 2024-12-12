9af07bd6b82e4dfc7fc8a8d487dd450f95364b79
Dina <dinagiacobbi@gmail.com>
2024-12-12 13:13:49 -0900

    Adding controllers and routers for reviews

---
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -0,0 +1,27 @@
+const createReview = async (req, res) => {
+  res.send('create review')
+}
+
+const getAllReviews = async (req, res) => {
+  res.send('get all reviews')
+}
+
+const getSingleReview = async (req, res) => {
+  res.send('get single review')
+}
+
+const updateReview = async (req, res) => {
+  res.send('review updated!')
+}
+
+const deleteReview = async (req, res) => {
+  res.send('review deleted!')
+}
+
+module.exports = {
+  createReview,
+  getAllReviews,
+  updateReview,
+  getSingleReview,
+  deleteReview,
+}
