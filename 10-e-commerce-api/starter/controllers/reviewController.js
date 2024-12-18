35fda49ed81a6a9e2fc1f4644bb364105ee8d631
Dina <dinagiacobbi@gmail.com>
2024-12-18 06:55:50 -0900

    Adding getAll Review and single review controller functions

--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -4,35 +4,48 @@ const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
-
 const createReview = async (req, res) => {
-  const { product:productId } = req.body
-  const isValidProduct = await Product.findOne( {_id: productId})
-  if(!isValidProduct){
-    throw new CustomError.NotFoundError(`No product with ID: ${productId}`)
+  const { product: productId } = req.body
+  const isValidProduct = await Product.findOne({ _id: productId })
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(
+      `No product with ID: ${productId}`
+    )
   }
 
-  const alreadySubmitted = await Review. findOne( {
-    product:productId, user:req.user.userId
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
   })
 
-  if(alreadySubmitted){
-    throw new CustomError.BadRequestError("Already submitted review for this product")
+  if (alreadySubmitted) {
+    throw new CustomError.BadRequestError(
+      'Already submitted review for this product'
+    )
   }
 
   // We have middleware, that's why we can do this:
   req.body.user = req.user.userId
   const review = await Review.create(req.body)
-  res.status(StatusCodes.CREATED).json( {review})
-
+  res.status(StatusCodes.CREATED).json({ review })
 }
 
 const getAllReviews = async (req, res) => {
-  res.send('get all reviews')
+  const reviews = await Review.find({})
+  res.status(StatusCodes.OK).json({ reviews, count: reviews.length })
 }
 
 const getSingleReview = async (req, res) => {
-  res.send('get single review')
+  const{id:reviewId } = req.params
+
+  const review = await Review.findOne( {_id: reviewId})
+
+  if(!review){
+    throw new CustomError.NotFoundError(
+      `No reviews with ID: ${reviewId} yet`
+    )
+  }
+  res.status(StatusCodes.OK).json({ review})
 }
 
 const updateReview = async (req, res) => {
