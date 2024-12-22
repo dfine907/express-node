1849f251474d0e6eb44f3abf2f6bb275666cb8af
Dina <dinagiacobbi@gmail.com>
2024-12-22 10:25:40 -0900

    Adding update controller function

--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -1,6 +1,6 @@
 const Review = require('../models/Review')
 const Product = require('../models/Product')
-const { checkPermissions } = require('../utils');
+const { checkPermissions } = require('../utils')
 
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
@@ -50,7 +50,25 @@ const getSingleReview = async (req, res) => {
 }
 
 const updateReview = async (req, res) => {
-  res.send('review updated!')
+  const { id: reviewId } = req.params
+  const { rating, title, comment } = req.body
+
+  const review = await Review.findOne({ _id: reviewId })
+
+  if (!review) {
+    throw new CustomError.NotFoundError(
+      `No reviews with ID: ${reviewId}`
+    )
+  }
+
+  checkPermissions(req.user, review.user)
+
+  review.rating = rating
+  review.title = title
+  review.comment = comment
+
+  await review.save()
+  res.status(StatusCodes.OK).json({ review })
 }
 
 const deleteReview = async (req, res) => {
@@ -64,9 +82,9 @@ const deleteReview = async (req, res) => {
     )
   }
 
-  checkPermissions(req.user, review.user);
-  await review.remove();
-  res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' });
+  checkPermissions(req.user, review.user)
+  await review.remove()
+  res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' })
 }
 
 module.exports = {
