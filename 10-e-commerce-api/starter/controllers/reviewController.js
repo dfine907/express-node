588a27695ba9c1f9816cb9a941c73f4a27b1c4df
Dina <dinagiacobbi@gmail.com>
2024-12-22 16:04:02 -0900

    Edits gettAllReviews controller

--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -31,8 +31,16 @@ const createReview = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ review })
 }
 
+// const getAllReviews = async (req, res) => {
+//   const reviews = await Review.find({})
+//   res.status(StatusCodes.OK).json({ reviews, count: reviews.length })
+// }
+
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({})
+  const reviews = await Review.find({}).populate({
+    path: 'product',
+    select: 'name company price',
+  })
   res.status(StatusCodes.OK).json({ reviews, count: reviews.length })
 }
 
