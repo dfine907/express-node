db22f74a561a556ecb965ad87e169b6bb7cfc992
Dina <dinagiacobbi@gmail.com>
2025-01-01 10:28:33 -0900

    Adding order controller and routes files

---
+++ 10-e-commerce-api/starter/controllers/orderController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User')
+const Product = require('../models/Product')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+const {
+  attachCookiesToResponse,
+  createTokenUser,
+} = require('../utils')
+
+const createOrder = async (req, res) => {
+  res.send('created order')
+}
+const getAllOrders = async (req, res) => {
+  res.send('get all orders')
+}
+const getSingleOrder = async (req, res) => {
+  res.send('get single order')
+}
+const getCurrentUserOrders = async (req, res) => {
+  res.send('get current user orders')
+}
+
+const updateOrder = async (req, res) => {
+  res.send('update order')
+}
+
+module.exports = {
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+}
